07e5f37ec6d0f6eb579bf12bb60c002643c38188
Cosmin Gherghe <cosminlinux@gmail.com>
2020-11-02 23:07:11 +0000

    Adding method parse out the document philadelphia_recreations.csv

--- src/main/java/dev/cosmingherghe/spark/Application.java
+++ src/main/java/dev/cosmingherghe/spark/Application.java
@@ -30,7 +30,9 @@ public class Application {
         durhamDf.show();
 
         // Parse out the document philadelphia_recreations.csv
-//        Dataset<Row> philadelphiaDf = buildPhiladelphiaParksDataFrame(spark);
+        Dataset<Row> philadelphiaDf = buildPhiladelphiaParksDataFrame(spark);
+        philadelphiaDf.printSchema();
+        philadelphiaDf.show();
 
 //        combineDataFrames(durhamDf, philadelphiaDf);
 
@@ -47,6 +49,7 @@ public class Application {
             .withColumn("park_name", df.col("fields.park_name"))
             .withColumn("city", lit("Durham"))
             .withColumn("has_playground", df.col("fields.playground"))
+            .withColumn("address", df.col("fields.address"))
             .withColumn("zip code", df.col("fields.zip"))
             .withColumn("land_in_acres", df.col("fields.acres"))
             .withColumn("geoX", df.col("geometry.coordinates").getItem(0))
@@ -58,6 +61,45 @@ public class Application {
             .drop("geometry")
             .drop("datasetid")
             .drop("record_timestamp");
+
+        return df;
+    }
+
+    private static Dataset<Row> buildPhiladelphiaParksDataFrame(SparkSession spark) {
+
+        Dataset<Row> df = spark.read().format("csv").option("multiline", true)
+                .option("header", true)
+                .load("src/main/resources/philadelphia_recreations.csv");
+
+//        df = df.filter(lower(df.col("USE_")).like("%park%"));
+        df = df.filter("lower(USE_) like '%park%' ");
+
+        df = df.withColumn("park_id", concat(lit("phil_"), df.col("OBJECTID")))
+            .withColumnRenamed("ASSET_NAME", "park_name")
+            .withColumn("city", lit("Philadelphia"))
+            .withColumnRenamed("ADDRESS", "address")
+            .withColumn("has_playground", lit("UNKNOWN"))
+            .withColumnRenamed("ZIPCODE",  "zipcode")
+            .withColumnRenamed("land_in_acres","ACREAGE")
+            .withColumn("geoX", lit("UNKNOWN"))
+            .withColumn("geoY", lit("UNKNOWN"))
+
+        //dropping all the old fields that we don't need anymore.
+            .drop("SITE_NAME")
+            .drop("OBJECTID")
+            .drop("CHILD_OF")
+            .drop("TYPE")
+            .drop("USE_")
+            .drop("DESCRIPTION")
+            .drop("SQ_FEET")
+            .drop("ALLIAS")
+            .drop("CHRONOLOGY")
+            .drop("NOTES")
+            .drop("EDITED_BY")
+            .drop("OCCUPANT")
+            .drop("TENANT")
+            .drop("LABEL")
+        ;
 
         return df;
     }
